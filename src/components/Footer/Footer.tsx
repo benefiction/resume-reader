26b00084e9d60e68972db5545f9f0812d4187a1a
Benedikt Villwock <github@benefiction.dev>
2022-02-23 12:56:46 +0100

    feat: add github actions for deploy to github page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,13 @@
 import GithubIcon from '@/assets/profile_github.svg';
 import { t } from '@/utils/translate';
 import React from 'react';
+import style from './Footer.module.css';
 
 export const Footer: React.FC = () => {
     return (
         <div>
             <a href='https://github.com/benefiction/resume-reader'>
-                <img src={GithubIcon} />
+                <img className={style.link_icon} src={GithubIcon} />
                 {t('FOOTER_REPO_NOTICE')}
             </a>
         </div>
