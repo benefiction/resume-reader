3a84cdf1c19d437d05d6a756b3404c1db29c915e
Benedikt Villwock <github@benefiction.dev>
2022-03-28 19:57:23 +0200

    fix(test): use the right test parm

--- src/utils/__tests__/getUrlParams.test.ts
+++ src/utils/__tests__/getUrlParams.test.ts
@@ -11,28 +11,31 @@ const setMockLocationSearch = (searchQuery) => {
     globalThis.window.location.search = searchQuery;
 };
 
+const testSamples: { queryString: string; result: string | undefined }[] = [
+    {
+        queryString: '',
+        result: undefined,
+    },
+    {
+        queryString: '?keyToTest&mock=mock1',
+        result: undefined,
+    },
+    {
+        queryString: '?keyToTest=2%sdfsfsf&mock=mock1',
+        result: undefined,
+    },
+    {
+        queryString: '?keyToTest=1&mock=mock1',
+        result: '1',
+    },
+];
+
 describe('getUrlParams', () => {
-    it('should return undefined without any url param', async () => {
-        setMockLocationSearch('');
-        const urlParam = getUrlParam('paramKey');
-        expect(urlParam).toBe(undefined);
-    });
-
-    it('should return undefined for params without value', async () => {
-        setMockLocationSearch('?paramKey0&paramKey1=1&paramKey2=2');
-        const urlParam = getUrlParam('paramKey0');
-        expect(urlParam).toBe(undefined);
-    });
-
-    it('should return undefined for invalid params', async () => {
-        setMockLocationSearch('?paramKey0=2%sdfsfsf');
-        const urlParam = getUrlParam('paramKey1');
-        expect(urlParam).toBe(undefined);
-    });
-
-    it('should return the value for params that have value', async () => {
-        setMockLocationSearch('?paramKey0&paramKey1=1&paramKey2=2');
-        const urlParam = getUrlParam('paramKey1');
-        expect(urlParam).toBe('1');
+    testSamples.forEach(({ queryString, result }) => {
+        it(`should return '${result}' with '${result}' as url param`, async () => {
+            setMockLocationSearch(queryString);
+            const urlParam = getUrlParam('keyToTest');
+            expect(urlParam).toBe(result);
+        });
     });
 });
