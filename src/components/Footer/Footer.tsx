c80de5651775fe9a757e0a178c0ac04fc63c4732
Benedikt Villwock <2339108+benefiction@users.noreply.github.com>
2022-04-14 08:57:18 +0200

    feat: improve footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,17 @@
 import GithubIcon from '@/assets/profile_github.svg';
 import { t } from '@/utils/translate';
 import React from 'react';
+import { version } from '../../../package.json';
 import style from './Footer.module.css';
 
 export const Footer: React.FC = () => {
   return (
     <div className={style.wrapper}>
-      <a href="https://github.com/benefiction/resume-reader">
-        <img className={style.link_icon} src={GithubIcon} />
+      <a className={style.link} href="https://github.com/benefiction/resume-reader">
         {t('FOOTER_REPO_NOTICE')}
+        <img className={style.link_icon} src={GithubIcon} />
       </a>
+      <p className={style.version_hint}>v{version}</p>
     </div>
   );
 };
--- src/components/Footer/__tests__/Footer.test.tsx
+++ src/components/Footer/__tests__/Footer.test.tsx
@@ -2,12 +2,18 @@ import { t } from '@/utils/translate';
 import { render } from '@testing-library/react';
 import React from 'react';
 import { Footer } from '../';
+import { version } from '../../../../package.json';
 
 describe('<Footer>', () => {
-  it('renders the component', () => {
-    const { getByText } = render(<Footer />);
+  it('should render the component', () => {
+    const { getByRole } = render(<Footer />);
+    const link = getByRole('link');
+    expect(link).toHaveTextContent(t('FOOTER_REPO_NOTICE').trim());
+  });
 
-    const link = getByText(t('FOOTER_REPO_NOTICE'));
+  it('should contain the current version', () => {
+    const { getByText } = render(<Footer />);
+    const link = getByText(`v${version}`);
     expect(link).toBeInTheDocument();
   });
 });
