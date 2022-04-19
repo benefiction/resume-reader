0ea9e86ba72048013a3b72df5dd3f2a073863de2
Benedikt Villwock <2339108+benefiction@users.noreply.github.com>
2022-04-19 10:15:12 +0200

    feat(contact section): add descriptive title for icons

--- src/components/SectionContact/SectionContact.tsx
+++ src/components/SectionContact/SectionContact.tsx
@@ -5,6 +5,7 @@ import iconWww from '@/assets/contact_www.svg';
 import { ProfileList } from '@/components/ProfileList';
 import { ResumeSection } from '@/components/ResumeSection';
 import { SvgAsMask } from '@/components/SvgAsMask';
+import { t } from '@/utils/translate';
 import React from 'react';
 import style from './SectionContact.module.css';
 import type { SectionContactProps } from './SectionContact.types';
@@ -16,7 +17,7 @@ export const SectionContact: React.FC<SectionContactProps> = ({ basics }) => {
         <ul>
           {basics.email && (
             <li className={style.detail}>
-              <SvgAsMask styleClass={style.icon} src={iconEmail} />
+              <SvgAsMask styleClass={style.icon} src={iconEmail} title={t('CONTACT_EMAIL')} />
               <a className={style.email} href={`mailto:${basics.email}`}>
                 {basics.email}
               </a>
@@ -24,20 +25,20 @@ export const SectionContact: React.FC<SectionContactProps> = ({ basics }) => {
           )}
           {basics.phone && (
             <li className={style.detail}>
-              <SvgAsMask styleClass={style.icon} src={iconPhone} />
+              <SvgAsMask styleClass={style.icon} src={iconPhone} title={t('CONTACT_PHONE')} />
               <a href={`tel:${basics.phone}`}>{basics.phone}</a>
             </li>
           )}
           {basics.website && (
             <li className={style.detail}>
-              <SvgAsMask styleClass={style.icon} src={iconWww} />
+              <SvgAsMask styleClass={style.icon} src={iconWww} title={t('CONTACT_WWW')} />
               <a href={basics.website}>{basics.website}</a>
             </li>
           )}
 
           {basics.location?.city && (
             <li className={style.detail}>
-              <SvgAsMask styleClass={style.icon} src={iconLocation} />
+              <SvgAsMask styleClass={style.icon} src={iconLocation} title={t('CONTACT_ADDRESS')} />
               <span>
                 {basics.location.address && (
                   <span className={style.address_detail}>{basics.location.address}</span>
